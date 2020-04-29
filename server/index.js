d87547f33121a07dbabe4efc9e2ac8c66a94dd6e
keganokami <400800m@gmail.com>
2020-04-29 16:33:16 +0900

    heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,26 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config  = require('./config/dev')
-const FakeDb = require('./fake-db')
+const config = require('./config')
+const SampleDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then (
+}).then(
   () => {
-    const fakeDb = new FakeDb();
-    fakeDb.initDb()
+    if(process.env.NODE_ENV !== 'production') {
+      const fakeDB = new SampleDb()
+      // fakeDB.initDb()
+    }
   }
 )
 
-const app = express();
+const app = express()
 
-app.use('/api/v1/produts', productRoutes)
+app.use('/api/v1/products', productRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
 
 const PORT = process.env.PORT || '3001'
 
-app.listen(PORT, function(){
-  console.log('I am running')
+app.listen(PORT, function() {
+  console.log('I am running!')
 })
