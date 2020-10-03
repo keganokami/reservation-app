5928052ad254207fe01b926f4390040ba801ddfa
keganokami <400800m@gmail.com>
2020-10-03 12:13:37 +0900

    画像の任意本数アップロード（サーバー側の実装完了）

--- src/app/product/product-post/product-post.component.ts
+++ src/app/product/product-post/product-post.component.ts
@@ -8,6 +8,7 @@ import { SelectPrefectures } from 'src/app/util/prefectures';
 import { FormArray, FormGroup, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
 
 interface Tokens {
   username: string;
@@ -21,7 +22,7 @@ interface Tokens {
 })
 export class ProductPostComponent implements OnInit {
   form: FormGroup;
-  
+
   errors: any;
   tokenData: string = localStorage.getItem('app-meta');
   username: string;
@@ -29,19 +30,12 @@ export class ProductPostComponent implements OnInit {
   createDate: Date;
   prefectures = SelectPrefectures;
 
-  firstUploadedFiles: Array<File> = null;
-  secondUploadedFiles: Array<File> = null;
-  thirdUploadedFiles: Array<File> = null;
-
-  firstWillUploadFIleName: string = 'ファイルを選択してください';
-  secondWillUploadFIleName: string = 'ファイルを選択してください';
-  thirdWillUploadFIleName: string = 'ファイルを選択してください';
   saveCoverImage1: any = null;
   saveCoverImage2: any = null;
   saveCoverImage3: any = null;
 
   isPosting: boolean = false;
-  posting:boolean = false;
+  posting: boolean = false;
 
   options = {
     maxHeight: 1080,
@@ -66,22 +60,6 @@ export class ProductPostComponent implements OnInit {
     this.addPostForm();
   }
 
-  // input=fileの書き戻し
-  resetFirstFileInput() {
-    this.firstWillUploadFIleName = 'ファイルを選択してください';
-    this.firstUploadedFiles = null;
-  }
-
-  resetSecondFileInput() {
-    this.secondWillUploadFIleName = 'ファイルを選択してください';
-    this.secondUploadedFiles = null;
-  }
-
-  resetThirdFileInput() {
-    this.thirdWillUploadFIleName = 'ファイルを選択してください';
-    this.secondUploadedFiles = null;
-  }
-
   getDataUrl(blobImage: Blob, options: Object): Promise<any> {
     return new Promise((resolve) => {
       loadImage(blobImage, (canvas) => {
@@ -90,34 +68,36 @@ export class ProductPostComponent implements OnInit {
     });
   }
 
+  // アップロードするファイル名のラベル表示
   fileChange(index: number, element) {
-    if (index === 0) {
-      this.firstfileChange(element);
-      const label = document.getElementById('inputGroupFileText0' + index);
-      label.innerText = this.firstWillUploadFIleName;
-    } else if (index === 1) {
-      this.secondFileChange(element);
-      const label = document.getElementById('inputGroupFileText0' + index);
-      label.innerText = this.secondWillUploadFIleName;
-    } else {
-      this.thirdFileChange(element);
-    }
+    const label = document.getElementById('inputGroupFileText0' + index);
+    label.innerText = this.checkFileAndgetFileName(index, element);
   }
 
-  firstfileChange(element) {
+  // ファイルチェックメソッド
+  checkFileAndgetFileName(index: number, element: any): string {
     const file = element.target.files[0];
     const fileName: string = element.target.files[0].name;
     if (!this.check_extension(this.get_extension(fileName))) {
-      this.firstWillUploadFIleName = '拡張子が不正です';
-      this.firstUploadedFiles = null;
-      return;
+      return '拡張子が不正です';
     }
+    this.CheckfileSizeAndgetInfo(file, index);
+    return fileName;
+  }
+
+  // ファイルサイズをチェックして規定のサイズであればリサイズし、問題なければbase64化する
+  CheckfileSizeAndgetInfo(file: File, index: number) {
     if (file.size >= 1000000) {
       loadImage.parseMetaData(file, (data) => {
-        console.log(file);
         this.getDataUrl(file, this.options)
           .then(result => {
-            this.saveCoverImage1 = result;
+            if (index == 0) {
+              this.saveCoverImage1 = result;
+            } else if (index == 1) {
+              this.saveCoverImage2 = result;
+            } else {
+              this.saveCoverImage3 = result;
+            }
             console.log(result);
           });
       });
@@ -125,69 +105,15 @@ export class ProductPostComponent implements OnInit {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        this.saveCoverImage1 = reader.result;
+        if (index == 0) {
+          this.saveCoverImage1 = reader.result;
+        } else if (index == 1) {
+          this.saveCoverImage2 = reader.result;
+        } else {
+          this.saveCoverImage3 = reader.result;
+        }
       };
     }
-
-    this.firstWillUploadFIleName = fileName;
-    this.firstUploadedFiles = element.target.files;
-  }
-
-  secondFileChange(element) {
-    const file = element.target.files[0];
-    const fileName: string = element.target.files[0].name;
-    if (!this.check_extension(this.get_extension(fileName))) {
-      this.secondWillUploadFIleName = '拡張子が不正です';
-      this.secondUploadedFiles = null;
-      return;
-    }
-    if (file.size >= 1000000) {
-      loadImage.parseMetaData(file, (data) => {
-        console.log(file);
-        this.getDataUrl(file, this.options)
-          .then(result => {
-            this.saveCoverImage2 = result;
-            console.log(result);
-          });
-      });
-    } else {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        this.saveCoverImage2 = reader.result;
-      };
-    }
-    this.secondWillUploadFIleName = fileName;
-    this.secondUploadedFiles = element.target.files;
-  }
-
-  thirdFileChange(element) {
-    const file = element.target.files[0];
-    const fileName: string = element.target.files[0].name;
-    if (!this.check_extension(this.get_extension(fileName))) {
-      this.thirdWillUploadFIleName = '拡張子が不正です';
-      this.thirdUploadedFiles = null;
-      return;
-    }
-    if (file.size >= 1000000) {
-      loadImage.parseMetaData(file, (data) => {
-        console.log(file);
-        this.getDataUrl(file, this.options)
-          .then(result => {
-            this.saveCoverImage3 = result;
-            console.log(result);
-          });
-      });
-    } else {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        this.saveCoverImage3 = reader.result;
-      };
-    }
-
-    this.thirdWillUploadFIleName = fileName;
-    this.thirdUploadedFiles = element.target.files;
   }
 
   /**
@@ -200,7 +126,12 @@ export class ProductPostComponent implements OnInit {
     forms.postSize = this.postFroms.length;
     forms.coverImage1 = this.saveCoverImage1;
     forms.coverImage2 = this.saveCoverImage2;
-    forms.coverImage3 = this.saveCoverImage3;
+    if (forms.postSize == 2 && this.saveCoverImage2 == null) {
+      forms.coverImage2 = this.saveCoverImage3;
+    } else {
+      forms.coverImage2 = this.saveCoverImage2;
+      forms.coverImage3 = this.saveCoverImage3;
+    }
     forms.description1 = (document.getElementById('description0') as HTMLTextAreaElement).value;
     if (document.getElementById('description1') !== null) {
       forms.description2 = (document.getElementById('description1') as HTMLTextAreaElement).value;
@@ -272,10 +203,15 @@ export class ProductPostComponent implements OnInit {
   }
 
   addPostForm() {
-    this.postFroms.push(this.createPostFormGroup())
+    this.postFroms.push(this.createPostFormGroup()) 
+    setTimeout(() => {
+      const formIndex = this.postFroms.length - 1;
+      document.getElementById('description' + formIndex).focus();
+    }, 300); 
   }
-
   removePostFormGroup(index: number) {
     this.postFroms.removeAt(index)
+    const formIndex = index - 1;
+      document.getElementById('description' + formIndex).focus();
   }
 }
